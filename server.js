b56a28ca61597deda1f853d5a9c9fd205be80909
unknown <jaime.galindo@iteso.mx>
2025-02-15 21:11:50 -0600

    Modificacion en el puerto

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = require('./controllers/router');
 const loginRouter = require('./controllers/login_router');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
